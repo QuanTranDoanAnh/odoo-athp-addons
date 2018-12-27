75455fd88467cde8fb4e11e993ce86c5903a7880
QuanTranDoanAnh <td.anhquan@gmail.com>
2018-12-27 08:10:57 +0000

    change

--- athp_stock/models/product_product.py
+++ athp_stock/models/product_product.py
@@ -4,3 +4,5 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     partner_ext_code = fields.Char(string='Product Code (Partner)')
+    owner_id = fields.Many2one('res.partner', "Owned by",
+        domain="[('is_company','=',True)]")
